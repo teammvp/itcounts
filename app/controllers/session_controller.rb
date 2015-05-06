1789092b465e7aafeaa5fda49ec8fc4a9a6b9019
Luke Shillabeer <lshillabeer@gmail.com>
2015-05-07 03:05:16 +1000

    User implemented, some context added for logged-in state - User model, login, logout built - Seed file slightly edited - Category-goals-view changed to dynamically generate target and on-board values

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,6 @@ class SessionController < ApplicationController
 
   def create
     user = User.authenticate(params[:email], params[:password])
-
     if user
       session[:user_id] = user.id
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get   '/about'             => 'pages#about',      :as => 'about'
   get   '/login'             => 'session#new',     :as => 'login'
   post  '/login'             => 'session#create'
+  get   '/logout'            => 'session#destroy'
   get   '/categories'        => 'categories#index', :as => 'categories'
   get   '/:category'         => 'categories#show',  :as => 'single_category'
   get   '/goals/:goal_title' => 'goals#show',       :as => 'single_goal'
