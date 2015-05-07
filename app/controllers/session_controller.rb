107638ff2c9f8122428bc67edc76cf4bc1835515
Luke Shillabeer <lshillabeer@gmail.com>
2015-05-08 01:29:04 +1000

    Updated login and return-to-referral

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,15 +1,18 @@
 class SessionController < ApplicationController
   add_breadcrumb "Home", :root_path, :options => { :title => "Home" }
+ 
   # login form
   def new
     add_breadcrumb "Login", :login_path, :options => { :title => "Login" }
+
+    @referral_url = request.env['HTTP_REFERER']
   end
 
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to(params[:ref])
     else
       redirect_to login_path
     end
