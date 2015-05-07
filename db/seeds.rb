80a5f8aa4354784b5fb6076f6ad7b15a2252fb96
Luke Shillabeer <lshillabeer@gmail.com>
2015-05-07 16:44:14 +1000

    Added more user functionality (login, signup)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Goal.create([{ title: 'shower-timer', description: 'Use a timer in the shower today!',  duration: 1, multiplier: 5, target: 500, category_id: 1 }]);
-Goal.create([{ title: 'reduce-shower', description: 'Reduce your shower time by a minute today!',  duration: 1, multiplier: 5, target: 500, category_id: 1 }]);
-Goal.create([{ title: 'low-flow', description: 'Convert your "high flow" shower head to "low flow" this week!',  duration: 1, multiplier: 5, target: 500, category_id: 1 }]);
-Goal.create([{ title: 'leaky-taps', description: 'Fix a leaky tap, this week!.',  duration: 30, multiplier: 200, target: 1000, category_id: 1 }]);
-Goal.create([{ title: 'dont-drive', description: "Don't drive to work, today, catch a train, tram or ride a bike!", duration: 60, multiplier: 5, target: 500, category_id: 2 }]);
-Goal.create([{ title: 'lights-off',description: 'Turn the lights off when not being used, tonight!', duration: 20, multiplier: 5, target: 500, category_id: 3 }]);
-Goal.create([{ title: 'sensor-lights',description: 'Install some sensor lights outside or the garage this week!', duration: 20, multiplier: 5, target: 500, category_id: 3 }]);
-Goal.create([{ title: 'recycle-paper',description: 'Recycle your newspapers everyday!', duration: 2, multiplier: 5, target: 500, category_id: 4 }]);
-Goal.create([{ title: 'save-paper',description: 'Subscribe to digital editions of the newspaper today!', duration: 2, multiplier: 5, target: 500, category_id: 4 }]);
-Goal.create([{ title: 'recycle-food',description: 'Put your food scraps into compost everyday!', duration: 2, multiplier: 5, target: 500, category_id: 4 }]);
-Goal.create([{ title: 'recycle-water',description: 'Drain shower water into the gardens everyday!', duration: 2, multiplier: 5, target: 500, category_id: 4 }]);
-Goal.create([{ title: 'save-from-tap',description: 'Fill bucket with the cold water before the hot water begins!', duration: 2, multiplier: 5, target: 500, category_id: 4 }]);
+Goal.create([{ title: 'shower-timer', description: 'Set phone timer to 5 minutes while showering.',  duration: 1, multiplier: 60, target: 60, category_id: 1 }]);
+Goal.create([{ title: 'teeth-brushing', description: 'Turn off the tap while brushing your teeth.', duration:1, multiplier: 12, target: 100, category_id: 1 }])
+Goal.create([{ title: 'low-flow', description: 'Convert your "high flow" shower head to "low flow".',  duration: 1, multiplier: 5, target: 40, category_id: 1 }]);
+Goal.create([{ title: 'leaky-taps', description: 'Fix a leaky tap.',  duration: 30, multiplier: 200, target: 100, category_id: 1 }]);
+
+Goal.create([{ title: 'dont-drive', description: "Use public transport or cycle today.", duration: 1, multiplier: 5, target: 100, category_id: 2 }]);
+Goal.create([{ title: 'heater-use', description: "Turn central heating down 2 degrees.", duration: 1, multiplier: 5, target: 100, category_id: 2 }]);
+Goal.create([{ title: 'close-doors-and-windows', description: "Close doors and windows when heater is on.", duration: 1, multiplier: 5, target: 100, category_id: 2 }]);
+Goal.create([{ title: 'switch-provider', description: "Switch to a green energy-provider.", duration: 1, multiplier: 5, target: 100, category_id: 2 }]);
+
+Goal.create([{ title: 'energy-savings', description: 'Start changing to energy efficient bulbs.', duration: 20, multiplier: 5, target: 50, category_id: 3 }]);
+Goal.create([{ title: 'lights-off', description: 'Turn off a light that shouldn\'t be on.', duration: 20, multiplier: 5, target: 50, category_id: 3 }]);
+Goal.create([{ title: 'unplug-appliance', description: 'Unplug microwave when not in use.', duration: 20, multiplier: 5, target: 50, category_id: 3 }]);
+Goal.create([{ title: 'switch-off-computer', description: 'Shut down your computer when finished using it.', duration: 20, multiplier: 5, target: 50, category_id: 3 }]);
+
+Goal.create([{ title: 'recycle-paper',description: 'Recycle something you would have thrown out.', duration: 2, multiplier: 5, target: 30, category_id: 4 }]);
+Goal.create([{ title: 'save-cooking-water',description: 'Save the water you use while cooking to water plants.', duration: 2, multiplier: 5, target: 30, category_id: 4 }]);
+Goal.create([{ title: 'save-from-tap',description: 'Fill bucket with the cold water before the hot water begins!', duration: 2, multiplier: 5, target: 50, category_id: 4 }]);
 
 Category.create([{ title: 'water',description: 'water saving description', image_url: 'category_icons/water.svg' }]);
 Category.create([{ title: 'carbon',description: 'carbon saving description', image_url: 'category_icons/footprint.svg' }]);
@@ -40,6 +46,7 @@ User.create([{ username: 'JCLee', password: '123', email: 'us@us.com', password_
 User.create([{ username: 'JWolff', password: '123', email: 'some@some.com', password_salt: 'abcde' }]);
 User.create([{ username: 'LShillabeer', password: '123', email: 'who@who.com', password_salt: 'abcde' }]);
 User.create([{ username: 'AllOfUs', password: '123', email: 'where@where.com', password_salt: 'abcde' }]);
+User.create([{ username: 'admin', password: 'admin', email: 'admin@itcounts.com', password_salt: 'abcde' }]);
 
 UserGoal.create([{user_id: 1, goal_id: 1}, {user_id:1, goal_id: 2}]);
 UserGoal.create([{user_id: 2, goal_id: 1}, {user_id:2, goal_id: 3}, {user_id:2, goal_id: 4}]);
